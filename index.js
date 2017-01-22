e873a10b2c0e6f09d99abc85a070da490a76db61
Josh Schlesser <josh.schlesser@me.com>
2017-01-22 09:34:13 -0800

    added link support

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send(`referer: ${req.headers['referer']}`)
+  res.send(
+    `<html>
+      <head></head>
+      <body>
+        <p>${req.headers['referer']}</p>
+        <p><a href='http://http://echo-referer.herokuapp.com/click'>LINK</a></p>
+      </body>
+    </html>`
+  )
+})
+
+app.get('/click', function (req, res) {
+  res.send(
+    `<html>
+      <head></head>
+      <body>${req.headers['referer']}</body>
+    </html>`
+  )
 })
 
 app.listen(process.env.PORT || 9987, function () {
