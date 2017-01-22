2050b6c1a35c68fe8c9351c536b47ed362fae276
Josh Schlesser <josh.schlesser@me.com>
2017-01-22 09:54:15 -0800

    changed to return a script from /

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ var app = express()
 
 app.get('/', function (req, res) {
   res.send(
-    `<html>
-      <head></head>
-      <body>
-        <p>${req.headers['referer']}</p>
-        <p><a href='http://echo-referer.herokuapp.com/click'>LINK</a></p>
-      </body>
-    </html>`
+    `var p = document.createElement('p')
+     p.innerText = 'REFERER: ${req.headers['referer']}'
+     document.body.appendChild(p)
+
+     var a = document.createElement('a')
+     a.target = '_blank'
+     a.href = 'http://echo-referer.herokuapp.com/click'
+     a.text = 'LINK'
+     document.body.appendChild(a)
+    `
   )
 })
 
